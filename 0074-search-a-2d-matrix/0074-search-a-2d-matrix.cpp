ceb645560611012f1e4f0b0ed082c0f8f3d4ef35
Dishi Neekhra <dishineekhra23@gmail.com>
2026-06-27 11:31:21 +0530

    Time: 0 ms (100%), Space: 13.4 MB (44.27%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-
-        int m = matrix.size();
-        int n = matrix[0].size();
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        // ARRAY SOLUTION
+        /*int m = mat.size();
+        int n = mat[0].size();
 
         int i = 0;
         int j = n - 1;
 
         while(i < m && j >= 0) {
 
-            if(matrix[i][j] == target) {
+            if(mat[i][j] == target) {
                 return true;
             }
-            else if(matrix[i][j] > target) {
+            else if(mat[i][j] > target) {
                 j--;
             }
             else {
                 i++;
             }
-        }
+        } */
 
+
+        //BINARY SOLUTION (2d to 1d)
+        int n= mat.size();
+        int m= mat[0].size();
+        int low=0; int high= n*m-1;
+        while(low <= high) {
+            int mid= (low+high)/2;
+            int row= mid/m;
+            int col= mid%m;
+
+            if(mat[row][col] == target) return true;
+            else if (mat[row][col] < target) low=mid+1;
+            else high= mid-1;
+        }
         return false;
     }
 };
