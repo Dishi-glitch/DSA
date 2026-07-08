565631ea052ea6e11d7df8a8c0cd2cd83430af98
Dishi Neekhra <dishineekhra23@gmail.com>
2026-07-08 11:05:15 +0530

    Time: 0 ms (100%), Space: 14.1 MB (63.62%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -3,27 +3,26 @@ public:
     void solve(int index, vector<int>& candidates, int target,
                vector<int>& temp, vector<vector<int>>& ans) {
 
-        if (target == 0) {
+        if (target == 0) { // target achieved
             ans.push_back(temp);
             return;
         }
 
-        for (int i = index; i < candidates.size(); i++) {
-
-            // Skip duplicates
-            if (i > index && candidates[i] == candidates[i - 1])
-                continue;
-
-            if (candidates[i] > target)
-                break;
-
-            temp.push_back(candidates[i]);
-
-            // i + 1 because each element can be used only once
-            solve(i + 1, candidates, target - candidates[i], temp, ans);
-
-            temp.pop_back();
+        if (index == candidates.size() || target < 0) { //fully traversed still didn't achieve target
+            return;
         }
+
+        temp.push_back(candidates[index]); //take
+        solve(index + 1, candidates, target - candidates[index], temp, ans); //shifts to next number
+        temp.pop_back(); //for backtracking where we didn't pick
+
+        while (index + 1 < candidates.size() &&      
+        // index+1 can not exceed n and it should not be equal to index if it is its a dulpicate so skip
+               candidates[index] == candidates[index + 1]) {
+                index++;
+        }
+
+        solve(index + 1, candidates, target, temp, ans); //not pick so just the index increases
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
